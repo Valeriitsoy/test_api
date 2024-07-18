4cb03a265c5bd8031a8a379bbecb366718ff372c
Valerii Tsoy <17a0120@gmail.com>
2024-07-18 16:57:58 +0300

    0.1

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,4 +1,5 @@
 import allure
+import pytest
 from config.base_test import BaseTest
 
 
@@ -7,21 +8,26 @@ from config.base_test import BaseTest
 class TestPosts(BaseTest):
 
     @allure.title('Test GET request for post information')
+    @pytest.mark.smoke
     def test_get_posts(self):
         self.api_posts.get_posts()
 
     @allure.title('Test creating a new post by POST')
+    @pytest.mark.smoke
     def test_add_posts(self):
         self.api_posts.add_posts()
 
     @allure.title('Test editing a post by PUT')
+    @pytest.mark.regress
     def test_edit_posts(self):
         self.api_posts.edit_posts()
 
     @allure.title('Test GET request to comment on a post')
+    @pytest.mark.regress
     def test_get_comment_posts(self):
         self.api_posts.get_comment_posts()
 
     @allure.title('Test Deleting a post')
+    @pytest.mark.regress
     def test_delete_posts(self):
         self.api_posts.delete_posts()
